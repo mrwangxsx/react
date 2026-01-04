820fe6930494ff964e974dc8e99ac06f87ffbc2a
mrwangxsx <mrwangxsx@gmail.com>
2026-01-04 23:22:29 +0800

    star rating

--- 07-usepopcorn/src/index.js
+++ 07-usepopcorn/src/index.js
@@ -1,14 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
 // import "./index.css";
 // import App from "./App";
 import StarRating from "./StarRating";
+function Test() {
+  const [movieRating, setmovieRating] = useState(0);
+  return (
+    <div>
+      <StarRating onSetMovieRating={setmovieRating} />
+      <p>The movie was rating{movieRating}</p>
+    </div>
+  );
+}
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
     <StarRating maxRating={5} />
-    <StarRating maxRating={10} />
-    <StarRating />
+    <Test />
   </React.StrictMode>
 );
